1265e68096afb5e6a9085f279c2abdbe25e7fa3b
Tmoemes <meesvanleusden@gmail.com>
2023-04-13 19:05:56 +0200

    sound effects for walking

--- Puzzle-Platformer/Assets/Scripts/WalkingEffects.cs
+++ Puzzle-Platformer/Assets/Scripts/WalkingEffects.cs
@@ -1,3 +1,4 @@
+using Invector.vCharacterController;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,44 @@ public class WalkingEffects : MonoBehaviour
 {
     public AudioClip[] SoundEffects;
     private AudioSource _audioSource;
+    private bool _isPlaying;
+    private AudioClip _currentlyPlaying;
+    public vThirdPersonMotor MovementMotorScript;
     // Start is called before the first frame update
     void Start()
     {
+        _isPlaying = false;
         _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0)
+        if ((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !_isPlaying && MovementMotorScript.isGrounded)
         {
-            _audioSource.Play();
+            _audioSource.pitch = Random.Range(1f,1.3f);
+            _audioSource.volume = Random.Range(0.3f, 0.8f);
+
+            if (MovementMotorScript.isSprinting)
+            {
+                Debug.Log("Playing fast");
+                _currentlyPlaying = SoundEffects[1];
+            }
+            else
+            {
+                Debug.Log("Playing normal");
+                _currentlyPlaying = SoundEffects[0];
+            }
+            _audioSource.PlayOneShot(_currentlyPlaying);
+            _isPlaying = true;  
+            StartCoroutine(WaitForClip());
         }
     }
+
+    IEnumerator WaitForClip()
+    {
+        yield return new WaitForSeconds(_currentlyPlaying.length);
+        _isPlaying = false;
+    }
 }
+
